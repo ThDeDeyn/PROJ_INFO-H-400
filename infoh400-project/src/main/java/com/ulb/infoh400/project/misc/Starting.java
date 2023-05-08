6c574883c1591a8240c0417e1ee1464eadda18cb
ThDeDeyn <118108149+ThDeDeyn@users.noreply.github.com>
2023-05-08 23:37:31 +0200

    WindowsPI
    At this stage, some Windows have been made :
    
    - start Window
    - ID window (pat & doc)
    - Main Windows (pat & doc)

---
+++ infoh400-project/src/main/java/com/ulb/infoh400/project/misc/Starting.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ulb.infoh400.project.misc;
+
+import com.ulb.infoh400.project.view.StartWindow;
+
+/**
+ *
+ * @author thoma
+ */
+public class Starting {
+/*
+    public static void main(String[] args) {
+        System.out.println("Starting !");
+        StartWindow MW = new StartWindow(); 
+        MW.setVisible(true);
+    }
+*/
+}
---
+++ infoh400-project2023/src/main/java/com/mycompany/infoh400/project2023/Infoh400Project2023.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.infoh400.project2023;
+
+/**
+ *
+ * @author thoma
+ */
+public class Infoh400Project2023 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
